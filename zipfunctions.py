f7cec4e67017b0aa3427d06baebb275538fa655c
Marc Schlechter <marc@schlechter.nl>
2016-06-08 14:48:19 +0200

    even more cleanup

--- zipfunctions.py
+++ zipfunctions.py
@@ -2,19 +2,19 @@ import os
 import zipfile
 
 # Add directory to zip file
-def add_dir_to_zip(path: str, ziphandle: zipfile.ZipFile):
-    for root, dirs, files in os.walk(path):
+def add_dir_to_zip(zip_dir: str, zip_file: zipfile.ZipFile):
+    for root, dirs, files in os.walk(zip_dir):
         for file in files:
             absfilename = os.path.join(root, file)
-            arcname = os.path.relpath(absfilename, path)
-            ziphandle.write(absfilename, arcname)
+            arcname = os.path.relpath(absfilename, zip_dir)
+            zip_file.write(absfilename, arcname)
         for dir in dirs:
             absfilename = os.path.join(root, dir)
-            arcname = os.path.relpath(absfilename, path)
-            ziphandle.write(absfilename, arcname)
+            arcname = os.path.relpath(absfilename, zip_dir)
+            zip_file.write(absfilename, arcname)
 
 # Create new zip file for directory
 def create_zip(filename: str, archive_dir: str):
-    zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-    add_dir_to_zip(archive_dir, zipf)
-    zipf.close()
+    zip_file = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
+    add_dir_to_zip(archive_dir, zip_file)
+    zip_file.close()
